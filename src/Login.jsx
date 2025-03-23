b7c89fea43fbfad9b6aac1b3941894ce2cdd21ab
Farah <farahezzine1919@gmail.com>
2025-03-23 22:57:30 +0100

    Test login

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import "./Login.css"; // Import du fichier CSS
+console.log("Login component loaded");
 
 const Login = () => {
   return (
     <div className="login-container">
       <div className="login-box">
-        <h1>welcome</h1>
-        <p>connectez-vous pour passer</p>
+        {/* Barre de navigation en haut */}
+        <div className="login-header">
+          <span className="login-link">Se connecter</span>
+          <span className="signup-link">S'inscrire</span>
+        </div>
+
+        <h1>Welcome</h1>
+        <p>Connectez-vous pour passer</p>
       </div>
     </div>
-    
   );
 };
 
 export default Login;
-
