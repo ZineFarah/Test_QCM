0e1bd022369d154c9fe6c2a8298f7fe11bc7583f
chayma503 <chaymabouzeienne@gmail.com>
2025-03-24 22:01:46 +0100

    signup

---
+++ src/signup.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Signup.css"; // Import du fichier CSS spécifique à la page d'inscription
+console.log("Signup component loaded");
+
+const Signup = () => {
+  return (
+    <div className="signup-container">
+      <div className="signup-box">
+        {/* Barre de navigation en haut */}
+        <div className="signup-header">
+          
+        </div>
+
+        <button className="signup-button">S'inscrire</button>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
