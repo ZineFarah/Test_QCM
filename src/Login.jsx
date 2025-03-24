e7a320a6e6e2b7898c69bc2801be5c7041171972
Farah <farahezzine1919@gmail.com>
2025-03-24 21:28:25 +0100

    login

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,22 +1,72 @@
-import React from "react";
-import "./Login.css"; // Import du fichier CSS
-console.log("Login component loaded");
+import React, { useState } from "react";
+import { AiOutlineMail, AiOutlineLock, AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai"; 
+import "./Login.css";
 
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <div className="login-container">
       <div className="login-box">
-        {/* Barre de navigation en haut */}
         <div className="login-header">
           <span className="login-link">Se connecter</span>
           <span className="signup-link">S'inscrire</span>
         </div>
-        <button className="login-button">Se connecter</button>
-
         
+
+        <div className="form-container">
+
+          {/* Email */}
+          <div className="email-container">
+            <div className="email-icon">
+             <AiOutlineMail  /> 
+            </div>
+            <div className="email-input">
+            <input type="email" placeholder="Email" />
+          </div>
+          </div>
+
+          {/* Mot de passe */}
+          <div className="password-container">
+            <div className="password-icon">
+              <AiOutlineLock />
+            </div>
+            <div className="password-input">
+              <input 
+                type={showPassword ? "text" : "password"} 
+                placeholder="Mot de passe" 
+              />
+            </div>
+            <div 
+              className="eye-icon" 
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
+            </div>
+          </div>
+          {/* Mot de passe oublié */}
+          <div className="forgot-password">
+               <a href="#">Mot de passe oublié ?</a>
+          </div>
+
+
+          {/* Bouton Se connecter */}
+          <div className="button-container">
+            <button className="login-button">Se connecter</button>
+        </div>
+
+        {/* Images */}
+        <div className="image-left">
+          <img src="./public/back1.png" alt="Illustration gauche" />
+        </div>
+
+        <div className="image-right">
+          <img src="./public/login.jpg" alt="Illustration droite" />
+        </div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default Login;
