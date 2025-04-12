10d6024af3662087e6373f39c8b40d95aaf57ee1
Farah <farahezzine1919@gmail.com>
2025-04-12 01:15:52 +0100

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Signup from "./signup";
+import Signup from "./Signup";
 import Login from "./Login";
 import Forgotpass from "./Forgotpass";
+import Welcome_admin from "./Welcome_admin";
 import Welcome from "./Welcome";
 import ManageAccount from "./ManageAccount";  // Composant pour gérer le compte
-
+import Welcome2 from "./Welcome2";
 
 
 function App() {
@@ -16,6 +17,8 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/forgotpass" element={<Forgotpass />} />
           <Route path="/welcome" element={<Welcome />} />
+          {/*<Route path="/Welcome_admin" element={<Welcome_admin />}/>*/}
+          <Route path="/welcome2" element={<Welcome2 />} />
           <Route path="/manage-account" element={<ManageAccount />} />
           
         </Routes>
